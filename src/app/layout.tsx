eaa0c03b49f42756a0c4523c31af75a57bccd203
Tai Cameron <tadc936@gmail.com>
2025-07-03 11:01:14 +0100

    microsoft

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head></head>
+      <body>
+        {children}
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "s92hpuqr8f");
+            `,
+          }}
+        />
+      </body>
 
       {/* Google Tag Scripts */}
       <Script
